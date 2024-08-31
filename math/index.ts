936668d050e5ccb8c9ad539e50a691c205354432
Diman-ko <dr.komarov@mail.ru>
2024-08-31 10:49:38 +0200

    feat: Firs commit

---
+++ math/index.ts
@@ -0,0 +1,14 @@
+console.log(1+3);
+export function sum(a: number, b: number): number {
+    return a + b
+};
+export function sub(a: number, b: number): number {
+    return a - b
+};
+export function mul(a: number, b: number): number {
+    return a * b
+};
+export function div(a: number, b: number): number {
+    return a / b
+};
+
---
+++ math/math.ts
@@ -0,0 +1,6 @@
+import {div, mul, sub, sum} from "./index"
+
+sum(1,2);
+sub(1,2);
+mul(1,2);
+div(1,2);
